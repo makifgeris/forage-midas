c6c14830a86b9d483c0e77e4699362d402572f8b
makifgeris <43251104+makifgeris@users.noreply.github.com>
2026-05-04 19:16:51 +0300

    Create TransactionListener.java

---
+++ src/main/java/com/jpmc/midascore/TransactionListener.java
@@ -0,0 +1,11 @@
+package com.jpmc.midascore;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+@Component
+public class TransactionListener {
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core")
+    public void onMessage(Transaction transaction) {
+    }
+}
